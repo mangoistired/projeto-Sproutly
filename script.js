fa419c6973cbd572d26bd3376fe153104f92653c
Max <mangoistired@gmail.com>
2026-05-25 13:50:34 -0300

    agora mostra mensagem de erro

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const editor = document.getElementById('editor');
 const bRodar = document.getElementById('b-rodar');
 const pSaida = document.getElementById('retorno');
 
+let pyodide;
+
+async function inicializar() {
+    pSaida.innerText="Carregando...";
+
+    pyodide = await loadPyodide({
+        stdout: (texto) => {
+            pSaida.innerText += texto + "\n";
+        } 
+    });
+
+    pSaida.innerText= "Pronto!";
+
+}
+
+inicializar();
+
 async function executar() {
     const codigo = editor.value;
     if (codigo.trim() == "") {
@@ -11,14 +28,12 @@ async function executar() {
 
     // pSaida.innerText = "Você rodou:\n" + codigo;
     pSaida.innerText = ""
-    let pyodide = await loadPyodide({
-        stdout: (texto) => {
-            pSaida.innerText += texto + "\n";
-        }
-});
 
-pyodide.runPython(codigo);
-
+    try {
+        await pyodide.runPythonAsync(codigo);
+    } catch(erro) {
+        pSaida.innerText="Erro de Sintaxe: \n" + erro;
+    }
 }
 
 bRodar = addEventListener('click', executar);
