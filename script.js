9d26e28e69dd0e5902c6e2d4bfa297159f8fe7e2
Max <mangoistired@gmail.com>
2026-05-25 13:04:54 -0300

    funcionando o interpretador

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const editor = document.getElementById('editor');
 const bRodar = document.getElementById('b-rodar');
 const pSaida = document.getElementById('retorno');
 
-function executar() {
+async function executar() {
     const codigo = editor.value;
     if (codigo.trim() == "") {
         pSaida.innerText = "Digite algo antes de rodar o código!";
         return;
     }
 
-    pSaida.innerText = "Você rodou:\n" + codigo;
+    // pSaida.innerText = "Você rodou:\n" + codigo;
+    pSaida.innerText = ""
+    let pyodide = await loadPyodide({
+        stdout: (texto) => {
+            pSaida.innerText += texto + "\n";
+        }
+});
+
+pyodide.runPython(codigo);
+
 }
 
 bRodar = addEventListener('click', executar);
